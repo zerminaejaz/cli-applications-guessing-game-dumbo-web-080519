91c836b7e63bcdcd3d35fc1ad06c07f6c6af408f
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-27 17:25:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   random_number = rand(1,7)
   prompt = puts "Guess a number"
-  users_guess= get.chomp!
+  users_guess= gets.chomp!
   if(users_guess == random_number)
     print "You guessed the correct number!"
   elseif users_guess != random_number
