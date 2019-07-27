8d13281f33dd97c491e9c1d8d8d439fc95ab3f3b
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-27 17:24:01 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,9 +6,7 @@ def run_guessing_game
     print "You guessed the correct number!"
   elseif users_guess != random_number
     print "Sorry! The computer guessed #{random_number}."
-  elseif users_guess == "exit"
-    print "Goodbye!"
   else
-    return ""
+    print "Goodbye!"
   end
 end
